6f38be0384815d7a1a3271f30c9a284538fde35f
Eric Hernandez <ejhernandez@apexsystems.com>
2024-01-22 23:41:15 -0600

    test email html

--- src/libs/sendMail.js
+++ src/libs/sendMail.js
@@ -7,6 +7,22 @@ import {
     MAIL_PASSWORD
 } from '../config';
 
+
+const html = `
+<div style="display: block; margin: 0px auto; width: 60%; background: #F0FBFF; height: 600px;">
+//header
+<div style="display:block;">
+
+  <img style="display: block; width: 30%; margin: 0px auto" src="./logo.png" alt="">
+</div>
+<div style="width: 100%;">
+  <a 
+    style="margin: 0px auto; text-decoration: none; background: #4F46E5; color: white; font-weight: bold; padding: 20px 40px 20px; border-radius: 10%;" 
+    href="https://fca-client-production.up.railway.app/">Iniciar sesion</a>
+</div>
+</div>
+`
+
 export const sendMail = async (options) => {
     try {
         const transporter = nodemailer.createTransport({
@@ -23,6 +39,7 @@ export const sendMail = async (options) => {
             to: options.email,
             subject: options.subject,
             text: options.message,
+            html: html
         };
         // Enviamos el correo electrónico
         const info = await transporter.sendMail(mailOptions);
